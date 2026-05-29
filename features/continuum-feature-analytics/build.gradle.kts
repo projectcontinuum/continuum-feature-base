1338109c3b3bef7bdff03762815d66ae9f8333e4
projectcontinuum <projectdevcontinuum@gmail.com>
2026-05-29 22:11:10 +0530

    CONTNM-85: use 2025.1.1 cloud version bu default

--- features/continuum-feature-analytics/build.gradle.kts
+++ features/continuum-feature-analytics/build.gradle.kts
@@ -8,7 +8,7 @@ version = property("featureVersion").toString()
 
 // get continuum platform version from root project properties
 val continuumPlatformVersion = property("continuumPlatformVersion").toString()
-val kotlinScriptingVersion = "2.1.0"
+val kotlinScriptingVersion = "2.2.21"
 
 continuum {
     continuumVersion.set(continuumPlatformVersion)
